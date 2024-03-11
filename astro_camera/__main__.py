abab3fdb7e2c0609ffa234baedb3f8cc1b6709f0
Gabe R <9311953+Marsfan@users.noreply.github.com>
2024-03-11 01:07:37 -0700

    Capture multiple

--- astro_camera/__main__.py
+++ astro_camera/__main__.py
@@ -1,14 +1,30 @@
 from picamera2 import Picamera2
+from picamera2.request import CompletedRequest
 from pprint import pprint
 from pathlib import Path
+from libcamera import controls
+import json
 # Picamera2.set_logging(Picamera2.DEBUG)
 picam2 = Picamera2()
 
-camera_config = picam2.create_still_configuration(lores={"size": (340, 480), "format": "YUV420"})
+preview_config = picam2.create_preview_configuration(lores={"size": (340, 480), "format": "YUV420"})
+camera_config = picam2.create_still_configuration(raw={}, display=None)
 
-picam2.configure(camera_config)
+picam2.configure(preview_config)
 
 picam2.start()
-picam2.capture_file(Path("test.jpg"))
-# Capture_metadata waits on a new frame to arrive
-pprint(picam2.capture_metadata())
+
+input("Press enter to capture")
+request: CompletedRequest  =picam2.switch_mode_and_capture_request(camera_config)
+
+
+request.save("main", "image.jpg")
+request.save_dng("image.dng")
+with open("metadata.json", "w") as file:
+    json.dump(request.get_metadata(), file)
+request.release()
+
+
+# picam2.controls["AeConstraintModeEnum"] = "Normal"
+# picam2.set_controls({"AeConstraintMode": "Normal"})
+# pprint(picam2.camera_controls)
--- astro_camera/datatypes.py
+++ astro_camera/datatypes.py
@@ -2,7 +2,7 @@
 # -*- coding: UTF-8 -*-
 """Data types for the program."""
 from typing import Optional, TypedDict
-from libcamera import Transform, ColorSpace
+from libcamera import Transform, ColorSpace, controls, Rectangle
 from typing import Any, Literal
 # TODO: Submit typings to Raspi repo
 
@@ -25,7 +25,45 @@ class CameraProperties(TypedDict):
     Accessed by the :py:attr:`Picamera2.camera_properties` property.
 
     """
-    # FIXME: Finish writing this, see Picameara document
+
+    ColorFilterArrangement: int  # TODO: Enum for translating this.
+    """Sensor Bayer Order."""
+
+
+    Location: int  # TODO: enum for tranlsating this
+    """Location on the device. Unused by Raspberry Pi"""
+
+    Model: str
+    """Name the sensor advertises itself as."""
+
+    PixelArrayActiveAreas: tuple[int, int, int, int]
+    """Active area of the pixel array.
+
+    Is in the form (x offset, y offset, width, height).
+    """
+
+    PixelArraySize: tuple[int, int]
+    """Size of the sensors active pixel area in (x, y)."""
+
+    Rotation: int
+    """Rotation of sensor relative to camera board in degrees.
+
+    Many RPi sensors are mounted upside down (i.e. 180degrees)
+
+    """
+
+    ScalerCropMaximum: tuple[int, int, int, int]
+    """Area within active area that will be read out by the camera in its current mode.
+
+    In the form (x_offset, y_offset, width, height)
+
+    """
+
+    SensorSensitivity: float
+    """Releative sensitivity of current camera mode compared to other modes."""
+
+    UnitCellSize: tuple[int, int]
+    """Sensor Pixel size in (x, y) nanometers."""
 
 
 class CameraControls(TypedDict):
@@ -39,6 +77,182 @@ class CameraControls(TypedDict):
         Checking for existince is recommended.
 
     """
+    AeContstraintMode: controls.AeConstraintModeEnum
+    """Constraint Mode of the AEC/AGC algorithm."""
+
+    AeEnable: bool  # FIXME: Seems to be a 3 elemnt tuple?
+    """Turn the AEC/AGC algorithm on and off."""
+
+    AeExposureMode: controls.AeExposureModeEnum
+    """Exposure mode of the AEC/AGC algorithm."""
+
+    AeFlickerMode: controls.AeFlickerModeEnum
+    """Flicker avoidance mode of the AEC/AGC algorithm."""
+
+    AeFlickerPeriod: int
+    """Flicker Period in microseconds."""
+
+    AeMeteringMode: controls.AeMeteringModeEnum
+    """Metering mode of the AEC/AGC algorithm."""
+
+    AfMetering: controls.AfMeteringEnum
+    """Where focus should be measured."""
+
+    AfMode: controls.AfModeEnum
+    """Autofocus Mode."""
+
+    AfPause: controls.AfPauseEnum
+    """Pause continuous autofocus.
+
+    Only has an effect wehen in continous autofocus mode.
+
+    """
+
+    AfRange: controls.AfRangeEnum
+    """Range of lens positions to search."""
+
+    AfSpeed: controls.AfSpeedEnum
+    """Speed of autofocus search."""
+
+    AfTrigger: controls.AfTriggerEnum
+    """Start an autofocus cycle."""
+
+    AfWindows: list[tuple[int, int, int, int]]
+    """Location of windows in the image to use for measuring focus.
+
+    This should be a list of rectanges in the image. Each rectangle is
+    represented by a tuple of [x_offset, y_offset, width, height].
+
+    Rectangles refer to the maximum scaler crop window.
+    See :py:attr:`CameraProperties.ScalerCropMaximum`
+
+    """
+
+    AnalogueGain: Any  # TODO
+    "Consult the camera_controls property."
+
+    AwbEnable: bool  # FIXME: Seems to be 3 element tuple?
+    """Turn on or off the auto white balance algorithm."""
+
+    AwbMode: controls.AwbModeEnum
+    """Set the mode of the AWB algorithm."""
+
+     Brightness: float
+    """Adjust image brightness from -1.0 to 1.0."""
+
+    ColourCorrectionMatrix: tuple[float, float, float, float, float, float, float, float, float]
+    """3x3 matrix used by the ISP to convert raw camera colors to sRGB. Read Only."""
+
+    ColourGains: tuple[float, float]
+    """Pair of color gains. First number is red gain, second is blue gain.
+
+    Setting this will disable AWB.
+
+    """
+
+    ColourTemperature: int
+    """Estimate of the color temperature in Kelvin of the current image. Read Only."""
+
+    Contrast: float
+    """The contrast of the image.
+
+    0.0: No Contrast
+    1.0: Default "normal" contrast.
+    Larger numbers increase contrast proportionately.
+
+    """
+
+    DigitalGain: float
+    """Digital gain to apply to image.
+
+    This cannot be set directly. Set
+    :py:attr:`CameraControls.AnalogueGain` and this will be updated to
+    add any additional gain that is needed.
+
+    """
+
+    ExposureTime: Any # FIXME: Consult camera_controls property
+    """Sensor Exposure time in microseconds."""
+
+    ExposureValue: float
+    """The exposure compensation in "stops"
+
+    Value should be between -8.0 and 8.0.
+
+    Negative values decrease brightness, positive values increase it.
+    0.0 is the base or "normal" value.
+
+    """
+
+    FrameDuration: int
+    """Amouont of time in us since previous camera frame.
+
+    Only found in image metadata. Framerate should be changed with the
+    :py:attr:`CameraControls.FrameDurationLimits` control.
+
+    """
+
+    FrameDurationLimits: Any # TODO: Consulte the camera_controls property
+    """Minimum and maximum time (in us) that the sensor can take to deliver a frame."""
+
+    HdrChannel: controls.HdrChannelEnum
+    """Which HDR Channel the current frame represents. Read Only."""
+
+
+    HdrMode: controls.HdrModeEnum
+    """Whether to run the camera in HDR mode, and in which mode.
+
+    This is not the in-camera HDR support by the Camera Module 3
+
+    Mostly only supported by the Pi 5 or later devices.
+
+    """
+
+    LensPosition: float
+    """Position of the lens in dioptres.
+
+    Dioptres are the reciprocal of the distance in meters.
+
+    """
+
+    Lux: int
+    """Estimate of the scene brightness in lux. Read only."""
+
+    NoiseReductionMode: controls.draft.NoiseReductionModeEnum
+    """The noise reduction mode to use.
+
+    Normally Picamera2 will select an appropriate mode automatically.
+
+    """
+
+    Saturation: float
+    """Color saturation to use. Max of 32
+
+    0.0: Grayscale
+    1.0: Normal color
+    Higher values produce more saturated colors.
+
+    """
+
+    ScalerCrop: Rectangle
+    """The portion of the image recieved from the sensor that is saved as an image."""
+
+    SensorTimestamp: int
+    """Time the frame was produced by the sensor in nanoseconds since boot. Read only."""
+
+    SensorBlackLevels: tuple[int, int, int, int]
+    """The black levels of the raw sensor image. Read Only."""
+
+    Sharpness: float
+    """Sharpness of the image. Max of 16.0.
+
+    0.0: No sharpening
+    1.0: Normal sharpening.
+    Higher leads to more sharpening.
+
+    """
+
+
     # FIXME: Finish writing this, see Picameara document
     # FIXME Indicate they might nott be present. Need to check typing docs
 
