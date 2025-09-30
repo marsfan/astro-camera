790c461ff11b9a6a14986db75f21668db1b35519
Gabe R. <9311953+marsfan@users.noreply.github.com>
2025-09-29 22:55:29 -0700

    Skeleton of CLI interface for package

--- src/astro_camera/__main__.py
+++ src/astro_camera/__main__.py
@@ -4,7 +4,30 @@ from argparse import ArgumentParser
 
 
 def main() -> None:
-    ...
+    """Run when called from command line."""
+    parser = ArgumentParser(description="Astrophotography Camera")
+
+    # FIXME: Replace with subcommands?
+    parser.add_argument(
+        "tool",
+        type=str,
+        choices=["webui"],
+        help="The tool to launch"
+    )
+
+    parser.add_argument(
+        "--camera",
+        "-c",
+        choices=["picam", "webcam", "dummy"],
+        default="picam",
+        help="Type of camera to launch with."
+    )
+    args = parser.parse_args()
+
+    if args.tool == "webui":
+        raise NotImplementedError
+    else:
+        raise ValueError("Unknown tool")
 
 
 if __name__ == "__main__":
